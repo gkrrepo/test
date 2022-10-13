d2b8ded263afb5c7ad6ddd066a999824e2064ec1
gkrrepo <115538017+gkrrepo@users.noreply.github.com>
2022-10-13 18:24:20 +0530

    Testing the state file

---
+++ instance.tf
@@ -0,0 +1,29 @@
+terraform{
+    backend "s3" {
+        bucket = "terraform-buk11"
+        encrypt = true
+        key = "terraform.tfstate"
+        region = "us-east-1"
+    }
+}
+
+resource "aws_instance" "web-server" {
+    ami                 = "ami-06489866022e12a14"
+    instance_type       = "t2.micro"
+    count               = 1
+    key_name            = "terraform"
+    security_groups     = ["${aws_security_group.web-server.name}"]
+    user_data = <<-EOF
+
+        #!/bin/bash
+        sudo su
+        yum update -y
+        yum install httpd -y
+        systemctl start httpd
+        systemctl enable httpd
+        echo "<html><h1>Hello World</p></h1>
+        EOF
+    tags = {
+    Name = "web-server"
+    }
+}
