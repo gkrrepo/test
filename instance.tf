186ac0db76903ddbb893762b8a80fa5a8cdc95df
gkrrepo <115538017+gkrrepo@users.noreply.github.com>
2022-10-14 09:13:37 +0530

    Create instance.tf

---
+++ instance.tf
@@ -0,0 +1,25 @@
+provider "aws"{
+        region = "us-east-1"
+    }
+
+
+resource "aws_instance" "web-server" {
+    ami                 = "ami-06489866022e12a14"
+    instance_type       = "t2.micro"
+    count               = 1
+    key_name            = "terraform"
+    security_groups     = ["sg-0280840b0426ff6ef"]
+    user_data = <<-EOF
+
+        #!/bin/bash
+        sudo su
+        yum update -y
+        yum install httpd -y
+        systemctl start httpd
+        systemctl enable httpd
+        echo "<html><h1>Hello World</p></h1>
+        EOF
+    tags = {
+    Name = "web-server"
+    }
+}
